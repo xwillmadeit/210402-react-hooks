833689eca8a048588a69cfb677f28f6dbf503c49
xwillmadeit <xwillmadeit@gmail.com>
2021-04-02 13:38:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ Number = React.memo(Number)
 function App() {
   const [count, setCount] = useState(0)
   const [number, setNumber] = useState(10)
-  // let data = { number } // 这样写 Number 组件也会重新渲染，因为 App 组件每次 render 都会生成新的 data 对象，Memo 使用的是浅比较
+  // let data = { number } // 这样写 Number 组件会重新渲染，因为 App 组件每次 render 都会生成新的 data 对象，Memo 使用的是浅比较
   let data = React.useMemo(() => ({ number }), [number]) // useMemo 可用于缓存对象
   // let addNumber = () => setNumber(number + 10) // 这样写 Number 组件会重新渲染，因为 App 组件每次 render 都会导致新的 addNumber 函数被创建
   let addNumber = React.useCallback(() => setNumber(number + 10), [number]) // useCallback 可用于缓存函数
